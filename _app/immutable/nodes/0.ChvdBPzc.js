8a648b8b98876efe770c83248e78fb77e18e514c
suaveforge-deploy <actions@users.noreply.github.com>
2026-09-23 14:44:59 +0000

    deploy jumprope 7c9b59a9c09f

---
+++ _app/immutable/nodes/0.ChvdBPzc.js
@@ -0,0 +1,1 @@
+import{C as e,W as t,lt as n,v as r,w as i}from"../chunks/ULwGnrmc.js";import"../chunks/xihTtKlq.js";var a=n({prerender:()=>!0,ssr:()=>!1});function o(n,a){var o=i(),s=t(o);r(s,()=>a.children),e(n,o)}export{o as component,a as universal};
